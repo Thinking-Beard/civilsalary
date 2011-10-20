357f9d7e1247028202d989e6745842e1a1119ff3
Paul Tyng <paul@paultyng.net>
2011-10-20 16:59:29 -0400

    html 5 boilerplate

---
+++ civilsalary.web/Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace civilsalary.web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult HandleError(Exception exception)
+        {
+            var http = exception as HttpException;
+
+            if (http != null)
+            {
+                Response.StatusCode = http.GetHttpCode();
+            }
+
+            switch(Response.StatusCode)
+            {
+                case 404:
+                    return View("Http404");
+                default:
+                    return View("General", exception);
+            }
+        }
+
+        public ActionResult CatchAll(string url)
+        {
+            return View("Http404");
+        }
+    }
+}
--- civilsalary.web/Global.asax.cs
+++ civilsalary.web/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using civilsalary.web.Controllers;
+using AttributeRouting;
 
 namespace civilsalary.web
 {
@@ -14,19 +16,39 @@ namespace civilsalary.web
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
         }
 
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
+            routes.MapAttributeRoutes();
 
+            routes.MapRoute(
+                "CatchAllRoute",
+                "{*url}",
+                new { controller = "Error", "CatchAll" });
+        }
+
+        protected void Application_Error()
+        {
+            var exception = Server.GetLastError();
+            var httpException = exception as HttpException;
+
+            Response.Clear();
+            Server.ClearError();
+
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error";
+            routeData.Values["action"] = "HandleError";
+            routeData.Values["exception"] = exception;
+
+            Response.TrySkipIisCustomErrors = true;
+            Response.StatusCode = 500;
+            
+            IController errorsController = new ErrorController();
+            var rc = new RequestContext(new HttpContextWrapper(Context), routeData);
+            errorsController.Execute(rc);
         }
 
         protected void Application_Start()
